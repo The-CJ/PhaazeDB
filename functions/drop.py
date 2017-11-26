6b5e1128f3086c3ceb6de74550951edee39c85c4
The-CJ <christoperjaekel@googlemail.com>
2017-11-26 16:26:52 +0100

    added drop function

--- functions/drop.py
+++ functions/drop.py
@@ -1,2 +1,26 @@
-def drop():
-	pass
+import os
+
+def drop(content):
+	table_name = content.get('name', None)
+	if table_name == None:
+		class r():
+			response = 406
+			content = b'{"error":"field: `name` missing"}'
+		return r
+
+	if not os.path.isfile("DATABASE/{}.phaazedb".format(table_name)):
+		class r():
+			response = 405
+			content = '{"error":"container `{tbn}` does not exists"}'.replace('{tbn}', table_name).encode("UTF-8")
+		return r
+
+	#everything ok, del db
+
+	path = "DATABASE/{}.phaazedb".format(table_name)
+
+	os.remove(path)
+
+	class r():
+		response = 202
+		content = b'{"msg":"successfull deleted"}'
+	return r
