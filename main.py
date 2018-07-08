f4136768758e7b6810a5c44c64034b72d06368c3
The-CJ <christoperjaekel@googlemail.com>
2018-07-09 01:20:50 +0200

    started rewrite for the Phaaze DB v2

--- main.py
+++ main.py
@@ -1,199 +1,134 @@
-import http.server
+import asyncio
+from aiohttp import web
 import json, time, os, threading
 
-from functions.create import create as create
-from functions.delete import delete as delete
-from functions.drop import drop as drop
-from functions.update import update as update
-from functions.insert import insert as insert
-from functions.select import select as select
-from functions.show import show as show
 
-from admin.website import website as website
+class DATABASE(object):
+	def __init__(self, config=dict()):
+		self.config = config
+		self.version = "2"
+		self.active = True
+		self.db = dict()
+		self.response = self.send_back_response
 
-DUMP = {}
-CLOSE = False
+	#functions
+	from functions.create import create as create
+	from functions.delete import delete as delete
+	from functions.drop import drop as drop
+	from functions.update import update as update
+	from functions.insert import insert as insert
+	from functions.select import select as select
+	from functions.show import show as show
 
-class RequestHandler(http.server.BaseHTTPRequestHandler):
+	#website
+	from admin.website import website as website
 
-	log = []
+	#errors
+	from utils.errors import unauthorised as unauthorised
+	from utils.errors import unknown_function as unknown_function
+	from utils.errors import missing_function as missing_function
 
-	def do_GET(self):
-		self.process()
+	def send_back_response(self, **kwargs):
+		already_set_header = kwargs.get('headers', dict())
+		kwargs['headers'] = already_set_header
+		kwargs['headers']['server'] =f"PhaazeDB v{self.version}"
+		kwargs['headers']['Content-Type'] ="Application/json"
+		r = web.Response(**kwargs)
+		return r
 
-	def do_POST(self):
-		self.process()
+	#accessable via web - /admin
+	async def interface(self, request):
+		return self.website()
 
-	def process(self):
-		global CLOSE
-		if CLOSE:
-			return
-
-		if self.path.startswith('/admin'):
-			r = website(self.path)
-			self.send_response(200)
-			self.end_headers()
-			if type(r) is not bytes:
-				r = r.encode('UTF-8')
-
-			self.wfile.write(r)
-			self.wfile.flush()
-			return
-
-
-		try: length = int(self.headers["Content-Length"])
-		except: length = 0
-
-		content = self.rfile.read(length)
+	#main entry call point
+	async def process(self, request):
+		#gather everything
+		_GET = request.query
+		_POST = dict()
+		_JSON = dict()
+		_HEADER = request.headers
 
 		try:
-			content = json.loads(content.decode("UTF-8"))
-		except:
-			content = None
+			_POST = await request.post()
+		except Exception as e:
+			pass
 
-		if content == None:
-			self.send_response(400)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(dict(status="error", msg="missing or corrupted content body")).encode("UTF-8"))
-			self.wfile.flush()
-			return
+		try:
+			_JSON = await request.json()
+		except Exception as e:
+			pass
 
-		token = content.get('token', None)
+		#get
+		token = request.query.get('token', None)
 
-		allow = False
-		if token == perms.get('auth_token', None):
-			allow = True
+		#post
+		if token == None: token = _POST.get('token', None)
 
-		if allow == False:
-			self.send_response(401)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(dict(status="error", msg="unauthorised")).encode("UTF-8"))
-			self.wfile.flush()
-			return
+		#json str
+		if token == None: token = _JSON.get('token', None)
 
-		action = content.get('action', None)
-		#get method
+		#header
+		if token == None: token = _HEADER.get('token', None)
+
+		# authorisation failed, block
+		if token != self.config.get('auth_token', None):
+			return await self.unauthorised()
+
+		#get action
+		action = _POST.get('action', None)
+		if action == None: action = _JSON.get('action', None)
+
 		if action == None:
-			self.send_response(400)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(dict(status="error", msg="missing 'action'")).encode("UTF-8"))
-			self.wfile.flush()
-			return
-
-		if action.lower() == "create":
-			rsp = create(content)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "delete":
-			rsp = delete(content, DUMP)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "drop":
-			rsp = drop(content, DUMP)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "update":
-			rsp = update(content, DUMP)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "insert":
-			rsp = insert(content, DUMP)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "select":
-			rsp = select(content, DUMP)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "show":
-			rsp = show(content, DUMP)
-
-			self.send_response(rsp.response)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(rsp.content)
-			self.wfile.flush()
-
-		elif action.lower() == "shutdown":
-			CLOSE = True
-			self.send_response(200)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(dict(status="success", msg="shutdown")).encode("UTF-8"))
-			self.wfile.flush()
-
-			t = shutdown()
-			t.start()
-
+			return await self.missing_function()
+		elif action == "create":
+			pass
+		elif action == "delete":
+			pass
+		elif action == "drop":
+			pass
+		elif action == "insert":
+			pass
+		elif action == "select":
+			pass
+		elif action == "show":
+			pass
+		elif action == "update":
+			pass
 		else:
-			self.send_response(406)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(json.dumps(dict(status="error", msg="method '{}' not supported".format(action))).encode("UTF-8"))
-			self.wfile.flush()
+			return await self.unknown_function()
 
-	def log_message(self, _format, *args):
-		return
+		return self.response()
 
-def webserver(perms):
-	server = http.server.HTTPServer(( perms.get("adress", "0.0.0.0"), perms.get("port", 1001) ), RequestHandler)
-	server.serve_forever()
+def start_server():
 
-class shutdown(threading.Thread):
-	def __init__(self):
-		super(shutdown, self).__init__()
+	#gather configs
+	try:
+		configs = open("config.json", "rb").read()
+		configs = json.loads(configs.decode("UTF-8"))
+	except:
+		print("`config.json` could not be found, or not read -> Using defaults WARNING: No PassToken.")
+		configs = dict()
 
-	def run(self):
-		time.sleep(5)
-		os._exit(1)
+	#check for DATABASE folder
+	try:
+		f = os.listdir('DATABASE/')
+	except:
+		os.mkdir('DATABASE')
 
-try:
-	perms = open("config.json", "rb").read()
-	perms = json.loads(perms.decode("UTF-8"))
-except:
-	print("`config.json` could not be found, or not read -> Using defaults WARNING: No PassToken.")
-	time.sleep(3)
-	perms = {"auth_token": ""}
+	#make app
+	DB = DATABASE(config=configs)
+	server = web.Application()
 
-#check for DATABASE
-try:
-	f = os.listdir('DATABASE/')
-except:
-	os.mkdir('DATABASE')
+	#web interface route
+	server.router.add_route('GET', '/admin{useless:.*}', DB.interface)
+	#main route
+	server.router.add_route('*', '/{useless:.*}', DB.process)
 
-print(open("logo.txt", "r").read())
-print("Running on port: "+str(perms.get("port", 1001)) + "\n")
+	#start
+	try:
+		print(open("logo.txt", "r").read())
+	except:
+		pass
+	web.run_app(server, port=configs.get('port', 1001))
 
-webserver(perms)
+start_server()
--- utils/errors.py
+++ utils/errors.py
@@ -4,3 +4,10 @@ async def unauthorised(self):
 	content = dict(code=401, status="error", msg="unauthorised")
 	return self.response(body=json.dumps(content))
 
+async def missing_function(self):
+	content = dict(code=400, status="error", msg="missing 'action' field")
+	return self.response(body=json.dumps(content))
+
+async def unknown_function(self):
+	content = dict(code=400, status="error", msg="unknown value for 'action'")
+	return self.response(body=json.dumps(content))
