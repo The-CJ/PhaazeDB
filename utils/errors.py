e760e544cf9bc74e4c6b0b90a2a9040ce3eb3175
The-CJ <christoperjaekel@googlemail.com>
2018-07-10 23:59:29 +0200

    added status to response

--- utils/errors.py
+++ utils/errors.py
@@ -2,12 +2,12 @@ import asyncio, json
 
 async def unauthorised(self):
 	content = dict(code=401, status="error", msg="unauthorised")
-	return self.response(body=json.dumps(content))
+	return self.response(status=401, body=json.dumps(content))
 
 async def missing_function(self):
 	content = dict(code=400, status="error", msg="missing 'action' field")
-	return self.response(body=json.dumps(content))
+	return self.response(status=400, body=json.dumps(content))
 
 async def unknown_function(self):
 	content = dict(code=400, status="error", msg="unknown value for 'action'")
-	return self.response(body=json.dumps(content))
+	return self.response(status=400, body=json.dumps(content))
