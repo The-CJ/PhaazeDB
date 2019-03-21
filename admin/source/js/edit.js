8795db523c3f39dcf6b11767c970f7c20ed7bf47
The_CJ <cj@tbits.net>
2019-03-21 11:05:39 +0100

    added detection if default is empty

--- admin/source/js/edit.js
+++ admin/source/js/edit.js
@@ -26,10 +26,37 @@ function describe() {
 
 function display_describe(data) {
   var result_space = $("#container_edit_modal .modal-result").html("");
-  let key_value_btn = $('<button type="button" class="btn btn-block btn-success">').text("Add");
+
+  result_space.append($("<h3>").text("Default: "+data.container));
+
+  default_template = data.default;
+  if (isEmpty(default_template)) {
+    result_space.append($("<h2>").text("No defaults set"));
+  }
+
+  let key_value_btn = $('<button type="button" class="btn btn-block btn-warning">').text("Add");
   key_value_btn.click(function () {
     add_key_value_field(result_space);
   })
-  $("#container_edit_modal .modal-result-footer").append(key_value_btn);
+  let save_btn = $('<button type="button" class="btn btn-block btn-success">').text("Save");
+  save_btn.click(function () {
+    set_default();
+  })
 
+  $("#container_edit_modal .modal-result-footer").html("").append(key_value_btn).append(save_btn);
+
+}
+
+function set_default() {
+  alert("reee")
+}
+
+function isEmpty(o) {
+  if (o == null) { return true; }
+  for (var v in o) {
+    if (o.hasOwnProperty(v)) {
+      return false
+    }
+  }
+  return true;
 }
