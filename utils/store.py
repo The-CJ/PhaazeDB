9de3f2aec9f810cc5b071c714a99538a75e60e67
The-CJ <christoperjaekel@googlemail.com>
2019-04-28 18:36:19 +0200

    store now has a create mode

--- utils/store.py
+++ utils/store.py
@@ -4,21 +4,28 @@ import pickle, threading, os
 lock = threading.Lock()
 locked_files = []
 
-async def store(self, container_name, container_content):
+async def store(self, container_name, container_content, create=False):
 
-	if not os.path.isfile(f"DATABASE/{container_name}.phaazedb"):
+	container_path = f"{self.container_root}{container_name}.phaazedb"
+
+	# is a new container, check for subfolder and create
+	if create:
+		os.makedirs(os.path.dirname(container_path), exist_ok=True)
+
+	# its not in create mode, but its trying to save to this file, return False
+	if not os.path.isfile(container_path) and not create:
 		return False
 
-	#file is already open -> wait until lock opened -> lock it + add name
+	# file is already open -> wait until lock opened -> lock it + add name
 	if container_name in locked_files:
 		lock.acquire()
 		locked_files.append(container_name)
-	#file is not opened -> lock without time delay + add name
+	# file is not opened -> lock without time delay + add name
 	else:
 		lock.acquire()
 		locked_files.append(container_name)
 
-	pickle.dump(container_content, open(f"DATABASE/{container_name}.phaazedb", "wb") )
+	pickle.dump(container_content, open(container_path, "wb") )
 
 	#operation finished -> remove name + release Lock
 	locked_files.remove(container_name)
