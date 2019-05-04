a3c6083f9ef3c134f5bc6369efdf77a452bd5c78
The-CJ <christoperjaekel@googlemail.com>
2019-05-04 18:57:32 +0200

    more stuff for update prepare

--- functions/update.py
+++ functions/update.py
@@ -1,5 +1,5 @@
 import json, math
-from utils.errors import MissingOfField, InvalidLimit, MissingUpdateContent
+from utils.errors import MissingOfField, InvalidLimit, MissingUpdateContent, SysLoadError, ContainerNotFound
 
 class UpdateRequest(object):
 	""" Contains informations for a valid update request,
@@ -77,7 +77,7 @@ async def update(self, request):
 		update_request = UpdateRequest(request.db_request)
 		return await performUpdate(self, update_request)
 
-	except (MissingOfField, InvalidLimit, MissingUpdateContent) as e:
+	except (MissingOfField, InvalidLimit, MissingUpdateContent, SysLoadError, ContainerNotFound) as e:
 		res = dict(
 			code = e.code,
 			status = e.status,
@@ -105,95 +105,33 @@ async def performUpdate(db_instance, update_request):
 
 async def updateDataInContainer(db_instance, update_request):
 
+	#unnamed key
 	if update_request.method == "dict" and update_request.content.get("", EmptyObject) != EmptyObject:
 		raise MissingUpdateContent(True)
 
-
-
-	if METHOD == "dict":
-		#unnamed dict key
-		if content.get('', dummy) != dummy:
-			res = dict(
-				code=400,
-				status="error",
-				msg="field 'content' has a unnamed json-object key"
-			)
-			return self.response(status=400, body=json.dumps(res))
-
-	#get where :: eval str
-	where = _INFO.get('_JSON', {}).get('where', None)
-	if where == None:
-		where = _INFO.get('_POST', {}).get('where', None)
-
-	if type(where) is not str:
-		where = None
-
-	#get offset :: int
-	offset = _INFO.get('_GET', {}).get('offset', None)
-	if offset == None:
-		offset = _INFO.get('_JSON', {}).get('offset', None)
-	if offset == None:
-		offset = _INFO.get('_POST', {}).get('offset', None)
-
-	if type(offset) is str:
-		if offset.isdigit():
-			offset = int(offset)
-	if type(offset) is not int:
-		offset = 0
-
-	#get limit :: int
-	limit = _INFO.get('_GET', {}).get('limit', None)
-	if limit == None:
-		limit = _INFO.get('_JSON', {}).get('limit', None)
-	if limit == None:
-		limit = _INFO.get('_POST', {}).get('limit', None)
-
-	if type(limit) is str:
-		if limit.isdigit():
-			limit = int(limit)
-	if type(limit) is not int:
-		limit = math.inf
-
 	#get current container from db
-	container = await self.load(table_name)
+	container = await db_instance.load(update_request.container)
 
 	#error handling
-	if container.status == "sys_error":
-		# this SHOULD never happen, but hey... just in case
-		res = dict(
-			code=500,
-			status="error",
-			msg="DB could not load container file."
-		)
-		return self.response(status=500, body=json.dumps(res))
+	if container.status == "sys_error": raise SysLoadError(update_request.container)
+	elif container.status == "not_found": raise ContainerNotFound(update_request.container)
+	elif container.status == "success":	container = container.content
 
-	elif container.status == "not_found":
-		res = dict(
-			code=404,
-			status="error",
-			msg=f"container '{table_name}' not found"
-		)
-		return self.response(status=404, body=json.dumps(res))
-
-	elif container.status == "success":
-
-		container = container.content
-
-	# # #
-
-	#search all entrys
+	#return values
 	hits = 0
+
+	#go through all entrys
 	found = 0
+	for entry_id in container.get('data', []):
+		entry = container['data'][entry_id]
+		entry['id'] = entry_id
 
-	for data_id in container.get('data', []):
-		data = container['data'][data_id]
-		data['id'] = data_id
-
-		if not await check_where(data, where):
+		# where dont hit entry, means skip, we dont need it
+		if not await checkWhere(where=select_request.where, check_entry=entry, check_name=select_request.store):
 			continue
 
 		found += 1
-		if offset >= found:
+		if update_request.offset >= found:
 			continue
 
 		if METHOD == "dict":
@@ -241,9 +179,14 @@ async def updateDataInContainer(db_instance, update_request):
 				self.logger.critical(f"update entry(s) from '{table_name}' failed")
 		return self.response(status=500, body=json.dumps(res))
 
-async def check_where(data, where):
-	if where == "" or where == None:
+async def checkWhere(where="", check_entry=None, check_name=None):
+
+	if not where:
 		return True
+
+	if not check_name:
+		check_name = "data"
+
 
 	try:
 		if eval(where):
