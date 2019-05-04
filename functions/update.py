1b465ae5fcff93fb5e27f0ef9038c7a186104539
The-CJ <christoperjaekel@googlemail.com>
2019-05-04 18:18:18 +0200

    main update request class and error catch

--- functions/update.py
+++ functions/update.py
@@ -1,54 +1,114 @@
-import asyncio, json, math
+import json, math
+from utils.errors import MissingOfField, InvalidLimit, MissingUpdateContent
 
-async def update(self, request, _INFO):
+class UpdateRequest(object):
+	""" Contains informations for a valid update request,
+		does not mean the container exists or where statement has right syntax """
+	def __init__(self, db_req):
+		self.container:str = None
+		self.where:str = ""
+		self.offset:int = 0
+		self.limit:int = math.inf
+		self.store:str = None
+		self.method:str = ""
+		self.content:all = None
+
+		self.getContainter(db_req)
+		self.getWhere(db_req)
+		self.getOffset(db_req)
+		self.getLimit(db_req)
+		self.getStore(db_req)
+		self.getUpdate(db_req)
+
+	def getContainter(self, db_req):
+		self.container = db_req.get("of", "")
+		if type(self.container) is not str:
+			self.container = str(self.container)
+
+		self.container = self.container.replace('..', '')
+		self.container = self.container.strip('/')
+
+		if not self.container: raise MissingOfField()
+
+	def getWhere(self, db_req):
+		self.where = db_req.get("where", "")
+
+	def getOffset(self, db_req):
+		self.offset = db_req.get("offset", -1)
+		if type(self.offset) is str:
+			if self.offset.isdigit():
+				self.offset = int(self.offset)
+
+		if type(self.offset) is not int:
+			self.offset = -1
+
+	def getLimit(self, db_req):
+		self.limit = db_req.get("limit", math.inf)
+		if type(self.limit) is str:
+			if self.limit.isdigit():
+				self.limit = int(self.limit)
+
+		if type(self.limit) is not int:
+			self.limit = math.inf
+
+		if self.limit <= 0:
+			raise InvalidLimit()
+
+	def getStore(self, db_req):
+		self.store = db_req.get("store", None)
+		if type(self.store) is not str:
+			self.store = None
+
+	def getUpdate(self, db_req):
+		self.content = db_req.get("content", None)
+
+		if type(self.content) is dict:
+			self.method = "dict"
+		elif type(self.content) is str:
+			self.method = "str"
+		else:
+			raise MissingUpdateContent()
+
+async def update(self, request):
 	""" Used to update entry fields in a existing container """
 
-	#get required vars (POST -> JSON based)
+	# prepare request for a valid search
+	try:
+		update_request = UpdateRequest(request.db_request)
+		return await performUpdate(self, update_request)
 
-	#get table_name
-	table_name = _INFO.get('_POST', {}).get('of', "")
-	if table_name == "":
-		table_name = _INFO.get('_JSON', {}).get('of', "")
-
-	if type(table_name) is not str:
-		table_name = str(table_name)
-
-	table_name = table_name.replace('..', '')
-	table_name = table_name.strip('/')
-
-	#no tabel name
-	if table_name == "":
+	except (MissingOfField, InvalidLimit, MissingUpdateContent) as e:
 		res = dict(
-			code=400,
-			status="error",
-			msg="missing 'of' field"
+			code = e.code,
+			status = e.status,
+			msg = e.msg()
 		)
-		return self.response(status=400, body=json.dumps(res))
+		return self.response(status=e.code, body=json.dumps(res))
 
-	#get content
-	content = _INFO.get('_POST', {}).get('content', None)
-	if content == None:
-		content = _INFO.get('_JSON', {}).get('content', None)
+	except Exception as ex:
+		return await self.criticalError(ex)
 
-	METHOD = None
+async def performUpdate(db_instance, update_request):
 
-	if type(content) is dict:
-		METHOD = "dict"
+	hits, total = updateDataInContainer(db_instance, update_request)
 
-	elif type(content) is str:
-		METHOD = "str"
+	res = dict(
+		code=201,
+		status="updated",
 
-	else:
-		content = None
+		hits=hits,
+		total=total,
+	)
+	if db_instance.Server.action_logging:
+		db_instance.Server.Logger.info(f"updated {str(hits)} entry(s) from '{update_request.container}'")
+	return db_instance.response(status=201, body=json.dumps(res))
 
-	#no content
-	if content == None:
-		res = dict(
-			code=400,
-			status="error",
-			msg="missing 'content' field as valid json-object or exec() string"
-		)
-		return self.response(status=400, body=json.dumps(res))
+async def updateDataInContainer(db_instance, update_request):
+
+	if update_request.method == "dict" and update_request.content.get("", EmptyObject) != EmptyObject:
+		raise MissingUpdateContent(True)
+
+
 
 	if METHOD == "dict":
 		#unnamed dict key
@@ -193,5 +253,4 @@ async def check_where(data, where):
 	except:
 		return False
 
-class dummy():
-	pass
+class EmptyObject(): pass
