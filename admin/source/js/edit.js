331484c3031a6d5dcf2b5db415f171fd0d724771
The-CJ <christoperjaekel@googlemail.com>
2019-05-08 16:50:35 +0200

    rewrotes

--- admin/source/js/edit.js
+++ admin/source/js/edit.js
@@ -31,7 +31,14 @@ class Describe {
   }
 
   build(data) {
-    var describe_result = _("[modal=edit] .modal-result").html("");
+    var describe_result = _("[modal=edit] [default-fields]").html("");
+    if (isEmpty(data)) {
+      describe_result.siblings(".modal-result-footer").hide();
+      return describe_result.addClass("empty");
+    } else {
+      describe_result.siblings(".modal-result-footer").show();
+      describe_result.removeClass("empty");
+    }
     for (var key in data) {
       let value = data[key];
 
@@ -55,55 +62,6 @@ class Describe {
   }
 }
 Describe = new Describe();
-
-function start_container_edit() {
-  $('#container_edit_modal').find('[name=container]').val( last_selected_container );
-  $('#container_edit_modal').find('.need_correction').removeClass('need_correction');
-  $('#container_edit_modal').modal('show');
-  curl['modal'] = 'container_edit';
-  update_curl();
-}
-
-function describe() {
-  let name = $('#container_edit_modal [name=container]').val();
-  let d = {
-    'action': 'describe',
-    'token': $('#db_token').val(),
-    'name': name
-  };
-  $.post('/', d)
-  .done(function (data) {
-    display_describe(data);
-  })
-  .fail(function (data) {
-    data = data.responseJSON ? data.responseJSON : {};
-    display_message({content:data.msg, color:"#fa3"});
-  })
-
-}
-
-function display_describe(data) {
-  var result_space = $("#container_edit_modal .modal-result").html("");
-
-  result_space.append($("<h3>").text("Default: "+data.container));
-
-  default_template = data.default;
-  if (isEmpty(default_template)) {
-    result_space.append($("<h2>").text("No defaults set"));
-  }
-
-  let key_value_btn = $('<button type="button" class="btn btn-block btn-warning">').text("Add");
-  key_value_btn.click(function () {
-    add_key_value_field(result_space);
-  })
-  let save_btn = $('<button type="button" class="btn btn-block btn-success">').text("Save");
-  save_btn.click(function () {
-    set_default();
-  })
-
-  $("#container_edit_modal .modal-result-footer").html("").append(key_value_btn).append(save_btn);
-
-}
 
 function set_default() {
   var new_default = {};
