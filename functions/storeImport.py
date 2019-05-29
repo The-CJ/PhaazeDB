265bd3cef37fcbf16e03ae1c683700837705a16b
The-CJ <christoperjaekel@googlemail.com>
2019-05-30 00:16:21 +0200

    stage before rework

--- functions/storeImport.py
+++ functions/storeImport.py
@@ -1,21 +1,19 @@
-import json
-import sys
+import json, asyncio
+from utils.errors import MissingImportFile
+
+
 
 async def storeImport(self, request):
 	"""
 		Used to import data from file. file extention should be .phzdb
 		but can be every type in therory
 	"""
-	content = await request.post()
-	db_file = content.get("phzdb", None)
-	print(db_file)
-
 	# prepare request for a valid search
 	try:
-		# update_request = UpdateRequest(request.db_request)
-		return await performImport(self, None)
 
-	except () as e:
+		return await performImport(self, request)
+
+	except (MissingImportFile) as e:
 		res = dict(
 			code = e.code,
 			status = e.status,
@@ -26,5 +24,35 @@ async def storeImport(self, request):
 	except Exception as ex:
 		return await self.criticalError(ex)
 
+class ImportManager(object):
+	""" lets call this a bridge class for all incomming data object to be proccessed and inserted into the right db """
+	def __init__(self, db):
+		self.db = db
+		self.current_db = None
+
+	async def processLine(self, line):
+		if line.startswith(b"ENTRY"): await self.proccessEntry(line)
+		elif line.startswith(b"DEFAULT"): await self.proccessDefault(line)
+		elif line.startswith(b"CONTAINER"): await self.proccessContainer(line)
+		else:
+			pass
+
+	async def proccessEntry(self, line):
+		print(line)
+
+	async def proccessDefault(self, line):
+		print(line)
+
+	async def proccessContainer(self, line):
+		print(line)
+
 async def performImport(db_instance, request):
-	pass
+	content = await request.post()
+	phzdb = content.get("phzdb", None)
+	if not phzdb:
+		raise MissingImportFile()
+
+	IM = ImportManager(db_instance)
+
+	for line in phzdb.file:
+		await IM.processLine(line)
