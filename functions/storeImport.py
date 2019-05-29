1a2ba4ba0ce5d0d8fd9d67514e72e8cc6c6e2242
The-CJ <christoperjaekel@googlemail.com>
2019-05-30 00:35:55 +0200

    new design in import

--- functions/storeImport.py
+++ functions/storeImport.py
@@ -1,35 +1,44 @@
 import json, asyncio
 from utils.errors import MissingImportFile
 
-
-
-async def storeImport(self, request):
+class ImportRequest(object):
 	"""
-		Used to import data from file. file extention should be .phzdb
-		but can be every type in therory
+		Contains informations for a valid import request
+		does not mean other errors are impossible,
+		also it's a bridge class for all incomming data object,
+		to be proccessed and inserted into the right db
 	"""
-	# prepare request for a valid search
-	try:
+	def __init__(self, db_req):
+		self.fileObject:None = None
 
-		return await performImport(self, request)
+		self.overwrite_container = False
+		self.overwrite_entrys = False
+		self.ignore_errors = False
 
-	except (MissingImportFile) as e:
-		res = dict(
-			code = e.code,
-			status = e.status,
-			msg = e.msg()
-		)
-		return self.response(status=e.code, body=json.dumps(res))
+		self.getFile(db_req)
+		self.getOverrides(db_req)
+		self.getIgnore(db_req)
 
-	except Exception as ex:
-		return await self.criticalError(ex)
+	def getFile(self, db_req):
+		self.fileObject = db_req.get("phzdb", None)
+		if not self.fileObject: raise MissingImportFile()
+		print( type(self.fileObject) )
 
-class ImportManager(object):
-	""" lets call this a bridge class for all incomming data object to be proccessed and inserted into the right db """
-	def __init__(self, db):
-		self.db = db
-		self.current_db = None
+	def getOverrides(self, db_req):
+		self.overwrite_container = db_req.get("overwrite_container", None)
+		if type(self.overwrite_container) is not bool:
+			self.overwrite_container = bool(self.overwrite_container)
 
+		self.overwrite_entrys = db_req.get("overwrite_entrys", None)
+		if type(self.overwrite_entrys) is not bool:
+			self.overwrite_entrys = bool(self.overwrite_entrys)
+
+	def getIgnore(self, db_req):
+		self.ignore_errors = db_req.get("ignore_errors", None)
+		if type(self.ignore_errors) is not bool:
+			self.ignore_errors = bool(self.ignore_errors)
+
+	# following are "bride" functions
 	async def processLine(self, line):
 		if line.startswith(b"ENTRY"): await self.proccessEntry(line)
 		elif line.startswith(b"DEFAULT"): await self.proccessDefault(line)
@@ -46,13 +55,28 @@ class ImportManager(object):
 	async def proccessContainer(self, line):
 		print(line)
 
-async def performImport(db_instance, request):
-	content = await request.post()
-	phzdb = content.get("phzdb", None)
-	if not phzdb:
-		raise MissingImportFile()
+async def storeImport(self, request):
+	"""
+		Used to import data from file. file extention should be .phzdb
+		but can be every type in therory
+	"""
+	# prepare request for a valid search
+	try:
+		import_import_request = ImportRequest(request.db_request)
+		await import_import_request.extractPost()
+		return await performImport(self, import_import_request)
 
-	IM = ImportManager(db_instance)
+	except (MissingImportFile) as e:
+		res = dict(
+			code = e.code,
+			status = e.status,
+			msg = e.msg()
+		)
+		return self.response(status=e.code, body=json.dumps(res))
 
-	for line in phzdb.file:
-		await IM.processLine(line)
+	except Exception as ex:
+		return await self.criticalError(ex)
+
+async def performImport(db_instance, import_import_request):
+	for line in import_import_request.fileObject.file:
+		await import_import_request.processLine(line)
