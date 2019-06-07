f584b2026f816ddb361b8524e9dcb3e8bb2e1394
The_CJ <cj@tbits.net>
2019-06-07 14:54:31 +0200

    load and store optimized

--- utils/load.py
+++ utils/load.py
@@ -1,33 +1,36 @@
-import asyncio
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from utils.database import Database as PhaazeDatabase
 
+import asyncio
 import pickle, os
 from utils.container import Container
 
-async def load(self, container_name, only_already_loaded=False):
+async def load(cls:"PhaazeDatabase", container_name:str, only_already_loaded:bool=False):
 
-	already_loaded = self.db.get(container_name, None)
-	if already_loaded != None:
+	AlreadyLoadedContainer:Container = cls.db.get(container_name, None)
+	if AlreadyLoadedContainer != None:
 		# reset time
-		already_loaded.keep_alive_time_left = self.keep_alive
-		return already_loaded
+		AlreadyLoadedContainer.keep_alive_time_left = cls.keep_alive
+		return AlreadyLoadedContainer
 
 	# its not currently loaded in ram, don't load it, just give None back
 	if only_already_loaded: return None
 
 	#try to load
-	container_location = f"{self.container_root}{container_name}.phaazedb"
+	container_location:str = f"{cls.container_root}{container_name}.phaazedb"
 
 	#does not exist
 	if not os.path.isfile(container_location):
-		return Container(self, container_name, status="not_found")
+		return Container(cls, container_name, status="not_found")
 
 	try:
-		container = pickle.load(open(container_location, "rb"))
+		container_content:dict = pickle.load(open(container_location, "rb"))
 		#store in db, for X time -> unload from RAM
-		self.db[container_name] = Container(self, container_name, status="success", content=container, keep_alive=self.keep_alive)
-		asyncio.ensure_future(self.db[container_name].countDown())
-		return self.db[container_name]
+		cls.db[container_name] = Container(cls, container_name, status="success", content=container_content, keep_alive=cls.keep_alive)
+		asyncio.ensure_future(cls.db[container_name].countDown())
+		return cls.db[container_name]
 
 	except Exception as e:
-		self.Server.Logger.error(f"Error loading container '{container_name}': {str(e)}")
-		return Container(self, container_name, status="sys_error")
+		cls.PhaazeDBS.Logger.error(f"Error loading container '{container_name}': {str(e)}")
+		return Container(cls, container_name, status="sys_error")
--- utils/store.py
+++ utils/store.py
@@ -1,31 +1,29 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from utils.database import Database as PhaazeDatabase
+
 import pickle, threading, os
 from utils.container import Container
 
-lock = threading.Lock()
-locked_files = []
+Lock:threading.Lock = threading.Lock()
+locked_files:list = []
 
-async def store(self, container_name, container_content, create=False, ignore_save_limit=False):
+async def store(cls:"PhaazeDatabase", DBContainer:Container, create:bool=False, ignore_save_limit:bool=False) -> bool:
 
-	# get container info object from db, based on name, won't be avariable if new container must be created
-	container = self.db.get(container_name, None)
-	if container == None:
-		container = Container(self, container_name)
-
-	container.actions_since_save += 1
+	DBContainer.actions_since_save += 1
 
 	# one condtion = save
-	if (container.actions_since_save > self.save_interval and self.save_interval > -1) or ignore_save_limit or create:
-		success = await performStore(self, container_name, container_content, create)
+	if (DBContainer.actions_since_save > cls.save_interval and cls.save_interval > -1) or ignore_save_limit or create:
+		success = await performStore(cls, DBContainer, create)
 		# save successfull, reset save counter
-		if success: container.actions_since_save = 0
+		if success: DBContainer.actions_since_save = 0
 		return success
 	else:
 		return True
 
+async def performStore(cls:"PhaazeDatabase", DBContainer:Container, create:bool):
 
-async def performStore(db_instance, container_name, container_content, create):
-
-	container_path = f"{db_instance.container_root}{container_name}.phaazedb"
+	container_path:str = f"{cls.container_root}{DBContainer.name}.phaazedb"
 
 	# is a new container, check for subfolder and create
 	if create:
@@ -36,18 +34,24 @@ async def performStore(db_instance, container_name, container_content, create):
 		return False
 
 	# file is already open -> wait until lock opened -> lock it + add name
-	if container_name in locked_files:
-		lock.acquire()
-		locked_files.append(container_name)
+	if DBContainer.name in locked_files:
+		Lock.acquire()
+		locked_files.append(DBContainer.name)
 	# file is not opened -> lock without time delay + add name
 	else:
-		lock.acquire()
-		locked_files.append(container_name)
+		Lock.acquire()
+		locked_files.append(DBContainer.name)
 
-	pickle.dump(container_content, open(container_path, "wb") )
+	try:
+		pickle.dump(DBContainer.content, open(container_path, "wb") )
+		state:bool = True
 
-	#operation finished -> remove name + release Lock
-	locked_files.remove(container_name)
-	lock.release()
+	except Exception as e:
+		cls.PhaazeDBS.Logger.critical("Pickel failed while storing container: " + str(e))
+		state:bool = False
 
-	return True
+	finally:
+		#operation finished -> remove name + release Lock
+		locked_files.remove(DBContainer.name)
+		Lock.release()
+		return state
